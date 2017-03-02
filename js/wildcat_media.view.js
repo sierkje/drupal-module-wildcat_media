29dfa9bdfea6e11af28189528106a1b83fdf174b
Sierk Beij <sierk@cococollective.org>
2017-03-02 01:19:49 -0800

    Added basic config and code for masonry implementation.

--- js/wildcat_media.view.js
+++ js/wildcat_media.view.js
@@ -3,30 +3,52 @@
  */
 (function ($, Drupal) {
 
-  "use strict";
+    "use strict";
 
-  /**
-   * Registers behaviours related to view widget.
-   */
+    /**
+     * Registers behaviours related to view widget.
+     */
 
-  Drupal.behaviors.WildcatMediaLibraryView = {
-    attach: function (context, settings) {
-      $('.item-container').css("display", "inline-block");
-      $('.grid-item').once('bind-click-event').click(function () {
-        var input = $(this).find('.views-field-entity-browser-select input');
-        input.prop('checked', !input.prop('checked'));
-        if (input.prop('checked')) {
-          $(this).addClass('checked');
-          var render = $(this).find('.views-field-rendered-entity');
-          $(render).css('opacity',0.3);
+    Drupal.behaviors.WildcatMediaView = {
+        attach: function (context) {
+            var $view = $('.view-content');
+            $view.prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>').once();
+
+            // Indicate that images are loading.
+            $view.append('<div class="ajax-progress ajax-progress-fullscreen">&nbsp;</div>');
+            $view.imagesLoaded(function () {
+                // Save the scroll position.
+                var scroll = document.body.scrollTop;
+                // Remove old Masonry object if it exists. This allows modules like
+                // Views Infinite Scroll to function with File Browser.
+                if ($view.data('masonry')) {
+                    $view.masonry('destroy');
+                }
+                $view.masonry({
+                    columnWidth: '.grid-sizer',
+                    gutter: '.gutter-sizer',
+                    itemSelector: '.grid-item',
+                    percentPosition: true,
+                    isFitWidth:true
+                });
+                // Jump to the old scroll position.
+                document.body.scrollTop = scroll;
+                // Add a class to reveal the loaded images, which avoids FOUC.
+                $('.grid-item').addClass('item-style');
+                $view.find('.ajax-progress').remove();
+            });
+
+            $('.grid-item').once('bind-click-event').click(function () {
+                var input = $(this).find('.views-field-entity-browser-select input');
+                input.prop('checked', !input.prop('checked'));
+                if (input.prop('checked')) {
+                    $(this).addClass('checked');
+                }
+                else {
+                    $(this).removeClass('checked');
+                }
+            });
         }
-        else {
-          $(this).removeClass('checked');
-          var render = $(this).find('.views-field-rendered-entity');
-          $(render).css('opacity',1);
-        }
-      });
-    }
-  };
+    };
 
 }(jQuery, Drupal));
